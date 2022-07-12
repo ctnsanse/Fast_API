b8c2edf22d6bfec0c328436e39d5292c7207eb36
Ctnsanse <cleytonsanse@gmail.com>
2022-07-13 01:55:11 +0200

    Basic API

--- pain.py
+++ pain.py
@@ -1,37 +1,10 @@
-from models import User, sexe, Role
-from uuid import uuid4, UUID
-from typing import List
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-db: List[User] = [
-    User(
-        id=UUID("d8c047b3-5acc-44cb-9a77-39ff8140bc88"),
-        first_name = "Cleyton",
-        last_name = "Sanse",
-        sexe = sexe.masculin,
-        roles = [Role.admin]
-    ),
-    User(
-        id=uuid4(),
-        first_name = "Duvensky",
-        last_name = "Ozil",
-        sexe = sexe.femminin,
-        roles = [Role.user, Role.helper]
-    )
-]
-
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/api/v1/users")
-async def fetch_users():
-    return db;
-
-
-@app.
+async def root():
+    return {"Hey": "mec"}
